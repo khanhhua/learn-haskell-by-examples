5960fe5b6661fc8085712816e47cbc869d157a3b
Khanh Hua <khanh.hua@doctari.de>
2021-08-23 00:42:24 +0200

    Monad with transformer

---
+++ GuestBook.hs
@@ -0,0 +1,65 @@
+module Main where
+
+import System.Exit
+import Control.Monad.State
+import Text.Printf
+import Data.List
+
+type Name = String
+type Age = Int
+type Entry = ( Name, Age )
+
+
+guestBook :: StateT [Entry] IO ()
+guestBook = do
+    input <- liftIO uiMenu
+    case input of 
+        '1' -> do
+            entries <- get
+            liftIO $ uiSearch entries
+            guestBook
+        '2' -> do
+            entry <- liftIO uiEnterNewEntry
+            entries <- get
+            put (entry : entries)
+            guestBook
+        'q' ->
+            liftIO $ exitSuccess 
+        _ ->
+            guestBook
+    
+
+
+uiMenu :: IO Char
+uiMenu = do
+    putStrLn "\n== GUESTBOOK =="
+    putStrLn "1. Search"
+    putStrLn "2. Enter new entry"
+    putStrLn "q. Quit\n"
+    getChar
+
+
+uiSearch :: [Entry] -> IO ()
+uiSearch entries = do
+    putStrLn "\n== SEARCH =="
+    putStr "Keyword: "
+    keyword <- getLine
+    let filtered = filter (\(name, _) -> keyword `isPrefixOf` name) entries
+    putStrLn $ printf "Found: %d" (length filtered)
+
+
+uiEnterNewEntry :: IO Entry
+uiEnterNewEntry = do
+    putStrLn "\n== NEW ENTRY =="
+    putStr "Name: "
+    name <- getLine
+    putStr "Age: "
+    ageS <- getLine
+
+    return ( name, read ageS :: Int )
+    
+
+main :: IO ()
+main = 
+    let initialState = []
+    in runStateT guestBook initialState >> return ()
