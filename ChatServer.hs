173b76ec210e92ba88a32984a6d8495636a46e79
Khanh Hua <khanh.hua@doctari.de>
2021-09-19 19:36:20 +0200

    Add ChatServer

--- ChatServer.hs
+++ ChatServer.hs
@@ -1,5 +1,17 @@
-module Main (main) where
+{- ChatServer
+- Accepts connection from telnet clients
+- Receives text message from client socket
+- Relay text message to other connected client threads via Chan String
+- Receives message from Chan and send back to client socket
 
+References:
+- https://hackage.haskell.org/package/network-3.1.1.1/docs/Network-Socket.html
+- https://wiki.haskell.org/Implement_a_chat_server
+-}
+module Main (main) where
+import System.IO
+
+import Control.Concurrent
 import Control.Monad.State
 import Control.Concurrent (forkFinally)
 import qualified Control.Exception as E
@@ -9,43 +21,45 @@ import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
 import qualified Data.ByteString.Char8 as Bytechar
 
-data ChatClient = ChatClient { sockClient :: Socket, chats :: [String] }
+data ChatClient = ChatClient { sockClient :: Socket, channel :: Chan String }
+
+chatClient :: StateT ChatClient IO ()
+chatClient = do
+  sock <- gets sockClient
+  sharedChan <- gets channel
+  duplicatedChan <- lift $ dupChan sharedChan
+  let broadcast msg = writeChan duplicatedChan msg
+
+  lift $ forkIO $ fix $ \loop -> do
+    line <- readChan duplicatedChan
+    sendAll sock (Bytechar.pack $ ">> " ++ line)
+    loop
+
+  lift $ fix $ \loop -> do
+    line <- recv sock 1024
+    broadcast (Bytechar.unpack line)
+    loop
 
 main :: IO ()
 main = runTCPServer Nothing "3000" talk
   where
-    -- talk s :: StateT ChatClient IO ()
-    talk s =
-      let initialState = ChatClient { sockClient = s, chats = [] }
-      in runStateT talk' initialState >> return ()
-
-    talk' :: StateT ChatClient IO ()
-    talk' = do
-      -- let s = sockClient state
-      s <- sockClient <$> get
-      chatItems <- chats <$> get
-
-      msg <- liftIO $ recv s 1024
-
-      unless (S.null msg) $ do
-        liftIO $ putStrLn (show chatItems)
-        put ChatClient { sockClient = s, chats = [(Bytechar.unpack msg)] ++ chatItems }
-        liftIO $ do
-          Bytechar.putStrLn msg
-          sendAll s msg
-      talk'
+    -- talk s :: StateT ChatClient IO String
+    talk channel s =
+      let initialState = ChatClient { sockClient = s, channel = channel }
+      in evalStateT chatClient initialState
 
 
 -- from the "network-run" package.
-runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO ()) -> IO ()
+runTCPServer :: Maybe HostName -> ServiceName -> (Chan String -> Socket -> IO ()) -> IO ()
 runTCPServer mhost port server = withSocketsDo $ do
   addr <- resolve
+  chan <- newChan -- notice that newChan :: IO (Chan a)
   -- bracket :: IO a -> (a -> IO b) -> (a -> IO c) -> IO c
-  -- brack is the monad exception handling: do (IO a) and finally release (a -> IO b)
+  -- bracket is the monad exception handling: do (IO a) and finally release (a -> IO b)
   --   the main action is (a -> IO c)
   -- expressed in try..catch as
   -- try (IO a) return (IO c) catch (IO b)
-  E.bracket (open addr) close loop
+  E.bracket (open addr) close (loop chan)
   where
     resolve = do
       let hints = defaultHints { addrFlags = [AI_PASSIVE], addrSocketType = Stream }
@@ -61,6 +75,6 @@ runTCPServer mhost port server = withSocketsDo $ do
       bind sock $ addrAddress addr
       listen sock 1024
       return sock
-    loop sock = forever $ do
+    loop chan sock = forever $ do
       (conn, _peer) <- accept sock
-      void $ forkFinally (void $ server conn) (const $ gracefulClose conn 5000)
+      void $ forkFinally (void $ server chan conn) (const $ gracefulClose conn 5000)
